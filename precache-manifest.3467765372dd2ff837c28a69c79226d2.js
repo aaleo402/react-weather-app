b5d4050dc377d15a6232fec962a9710a43389521
JsTraveller <34063422+JStraveller@users.noreply.github.com>
2020-02-21 10:31:39 +0300

    Updates

---
+++ precache-manifest.3467765372dd2ff837c28a69c79226d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb356371c9345e7e8010045f84cf094",
+    "url": "/JStraveller/react-weather-app/index.html"
+  },
+  {
+    "revision": "a981eb67b45d5b33f935",
+    "url": "/JStraveller/react-weather-app/static/css/main.85a14e96.chunk.css"
+  },
+  {
+    "revision": "6ed6d85e3b5b6eccf0ea",
+    "url": "/JStraveller/react-weather-app/static/js/2.6771613c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/JStraveller/react-weather-app/static/js/2.6771613c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a981eb67b45d5b33f935",
+    "url": "/JStraveller/react-weather-app/static/js/main.38029003.chunk.js"
+  },
+  {
+    "revision": "a4d6580eee91ad9a8e4f",
+    "url": "/JStraveller/react-weather-app/static/js/runtime-main.5dda5a43.js"
+  }
+]);
